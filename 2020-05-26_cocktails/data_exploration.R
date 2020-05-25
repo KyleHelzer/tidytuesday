28982296e6fe174ac55ed3ecca173615ec41ad04
Kyle Helzer <helzerk@gmail.com>
2020-05-24 22:48:24 -0500

    initial data exploration

---
+++ 2020-05-26_cocktails/data_exploration.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+
+# get the data
+
+cocktails <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-26/cocktails.csv')
+boston_cocktails <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-26/boston_cocktails.csv')
+
+glimpse(cocktails)
+
+# sort by number of incredients
+cocktails %>% 
+    group_by(drink) %>% 
+    summarise(
+        n = n()
+    ) %>% 
+    arrange(desc(n)) # top is Angelica Liqueur with 12 ingredients
+
+# find cocktails
+cocktails %>% 
+    group_by(drink) %>% 
+    summarise(
+        n = n()
+    ) %>% 
+    filter(n == 1)
+
+cocktails %>% 
+    filter(drink == "410 Gone") %>% 
+    select(drink, ingredient_number, ingredient, measure) # this is missing Coca Cola
+
+cocktails %>% 
+    filter(drink == "Screwdriver") %>% 
+    select(drink, ingredient_number, ingredient, measure) # this is missing Orange Juice
+
+cocktails %>% 
+    filter(drink == "Spiking coffee") %>% 
+    select(drink, ingredient_number, ingredient, measure) # this is missing alcohol
+
+cocktails %>% 
+    filter(drink == "Tequila Sunrise") %>% 
+    select(drink, ingredient_number, ingredient, measure) # this is missing Orange Juice and Grenadine
+
+
